6312fe809154e9e580fab26bdbcd38dc31a3b414
KimKabin <kabinny@gmail.com>
2023-08-24 14:45:05 +0900

    sw add eventListener

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,9 @@
 importScripts("https://cdn.pushalert.co/sw-60727.js");
+
+self.addEventListener('push', function (event) {
+    console.log('push event', event)
+})
+
+self.addEventListener('notificationclick', function (event) {
+    console.log('notificationclick event', event)
+})
