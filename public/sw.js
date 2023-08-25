12ac9dd3f7ef5e431451c7c288d24cefe43d6cad
KimKabin <kabinny@gmail.com>
2023-08-25 11:13:20 +0900

    alert 오류 삭제

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,10 @@
 importScripts("https://cdn.pushalert.co/sw-60727.js");
 
 self.addEventListener('push', function (event) {
-    alert(event)
     console.log('push event', event)
 })
 
 self.addEventListener('notificationclick', function (event) {
-    alert(event)
     console.log('notificationclick event', event)
 })
 
