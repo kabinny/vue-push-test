8dcd3a2296234e081b23c41ed0f9c4cd5ad9a6a4
KimKabin <kabinny@gmail.com>
2023-08-25 11:18:54 +0900

    request 삭제

--- src/page/MainPage.vue
+++ src/page/MainPage.vue
@@ -3,7 +3,6 @@
     <h2>메인 페이지</h2>
     <h3>Notification.permission</h3>
     <p>{{ userPermission }}</p>
-    <button @click="updatePermission">허용 상태 갱신</button>
   </div>
 </template>
 
@@ -19,16 +18,6 @@ export default {
     this.userPermission = Notification.permission
   },
   methods: {
-    requestPermission() {
-      if (!('Notification' in window)) {
-        alert('알림 지원 안됨')
-        return
-      }
-      Notification.requestPermission(status => {
-        this.userPermission = status
-      })
-
-    }
   }
 };
 </script>
