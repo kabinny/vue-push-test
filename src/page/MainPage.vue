fd1e67b512fe5f48d63cd5011ec209a4be54a6c7
KimKabin <kabinny@gmail.com>
2023-08-25 10:01:34 +0900

    Add user permission state

--- src/page/MainPage.vue
+++ src/page/MainPage.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
     <h2>메인 페이지</h2>
+    <h3>Notification.permission</h3>
+    <p>{{ userPermission }}</p>
+    <button @click="updatePermission">허용 상태 갱신</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "MainPage",
+  data() {
+    return {
+      userPermission: null
+    }
+  },
+  mounted() {
+    this.requestPermission()
+  },
+  methods: {
+    requestPermission() {
+      if ('Notification' in window && Notification && Notification.permission) {
+        this.userPermission = Notification.permission
+      }
+    }
+  }
 };
 </script>
