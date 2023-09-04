f2bb36ad3723cf465c6f2dec5eeb490bf7b04c04
KimKabin <kabinny@gmail.com>
2023-09-04 17:57:09 +0900

    async console log test

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,24 @@
 importScripts("https://cdn.pushalert.co/sw-60727.js");
 
+// console.log 를 비동기로 처리하기 위한 함수
+function asyncConsoleLog(message) {
+    return new Promise((resolve, reject) => {
+        console.log(message);
+        resolve();
+    });
+}
+
 self.addEventListener('push', function (event) {
-    console.log('push event', event)
+    asyncConsoleLog('push event')
+    // console.log('push event', event)
 })
 
 self.addEventListener('notificationclick', function (event) {
-    console.log('notificationclick event', event)
+    asyncConsoleLog('notificationclick event')
+    // console.log('notificationclick event', event)
 })
 
 self.addEventListener('activate', () => {
-    console.log('activate event')
+    asyncConsoleLog('activate event')
+    // console.log('activate event')
 })
